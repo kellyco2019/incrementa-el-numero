5564a6a530e54db0f1470a2c2011b46ea538470e
kellycora <kellyc@evollve.co>
2020-10-22 21:03:21 -0500

    incrementa

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  constructor () {
+    super();
+    
+    this.state = {
+      counter: 0
+    };
+
+    this.increaseValue = this.increaseValue.bind(this);
+  }
+    render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.counter}</span>
+        <button onClick={this.increaseValue} id="inc">Incrementa</button>
       </div>
-    );
+    )  
+  }
+    increaseValue() {
+    this.setState({
+      counter: 1
+    });
   }
 }
-
 export default App;
